a11f7342b30caaf1a10e0108dda2aba6d66025d7
sujinpark <sujinpark@sujinparkui-MacBook-Pro.local>
2020-11-10 10:08:41 +0900

    Updates

---
+++ precache-manifest.3aacefa5686b14c4f24c427a80bbd01e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "587d2dc69c638c0ac893e936daa3f9a7",
+    "url": "/wordbook/index.html"
+  },
+  {
+    "revision": "f8448011abc1092e962c",
+    "url": "/wordbook/static/css/main.6fa34873.chunk.css"
+  },
+  {
+    "revision": "f5520bb513bfea2fe0eb",
+    "url": "/wordbook/static/js/2.9299813e.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/wordbook/static/js/2.9299813e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8448011abc1092e962c",
+    "url": "/wordbook/static/js/main.cc505c4b.chunk.js"
+  },
+  {
+    "revision": "885acb5b0dd41f348cf4",
+    "url": "/wordbook/static/js/runtime-main.b00895fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wordbook/static/media/logo.5d5d9eef.svg"
+  }
+]);
