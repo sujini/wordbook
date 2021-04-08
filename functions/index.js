bfc634158f909fdca84858ae6b3a67c0639bc9e9
sujini0102 <ssujini0102@gmail.com>
2021-04-08 10:28:07 +0900

    modify firebase functions that insert to algolia data set

--- functions/index.js
+++ functions/index.js
@@ -10,30 +10,27 @@ const algoliasearch = require("algoliasearch");
 
 
 const env = functions.config();
-const ALGOLIA_ID = functions.config().algolia.app_id;
-const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
-const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY );
+const ALGOLIA_ID = env.algolia.app_id;
+const ALGOLIA_ADMIN_KEY = env.algolia.api_key;
+const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
 const wordsIndex = client.initIndex("words"); // <-- Index name
 
 exports.algoliaWordsSync = functions
     .firestore.document("words/{doc}").onWrite( (change, _context) => {
       const oldData = change.before;
       const newData = change.after;
-      const data = newData.data();
+      const {content, meaning} = newData.data();
       const objectID = newData.id; // <-- prop name is important
+      const data = {objectID, content, meaning};
 
       if (!oldData.exists && newData.exists) {
         // creating
-        return wordsIndex.saveObject(Object.assign({}, {
-          objectID,
-        }, data));
+        return wordsIndex.saveObject(data);
       } else if (!newData.exists && oldData.exists) {
         // deleting
         return wordsIndex.deleteObject(objectID);
       } else {
         // updating
-        return wordsIndex.saveObject(Object.assign({}, {
-          objectID,
-        }, data));
+        return wordsIndex.saveObject( data);
       }
     });
